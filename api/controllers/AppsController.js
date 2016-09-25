8f8a57d9010c1330d87f5eacbf15ef213ea97b2f
Shea Phillips <shea.phillips@gmail.com>
2016-09-25 16:34:33 -0700

    - enabled a bit of debug logging to help troubleshoot timeouts.

--- api/controllers/AppsController.js
+++ api/controllers/AppsController.js
@@ -1,48 +1,48 @@
-
-var github = require('octonode');
-var yaml = require('js-yaml');
 var getCached = false;
-var myCached = require ('../services/cached.json');
+var myCached = require('../services/cached.json');
 
-var aok = function (res) { return function (o) {res.json (o);}};
-var nok = function (res) { return function (e) {res.json (e);}};
+var aok = function (res) {
+    return function (o) {
+        sails.log.debug ('about to send results');
+        res.json (o);
+    }
+};
+
+var nok = function (res) {
+    return function (e) {
+        sails.log.debug ('about to send results');
+        res.json (e);
+    }
+};
 
 module.exports = {
 
-	openIssues: function (req, res) {
-		sails.log.debug("getCommits request body", req.param('widget'));
-		var widget = req.param('widget');
-		GithubService.getCommits(widget, function (err, data, headers) {
-			return res.json(data);
-		});
-	},
-	issues: function (req, res) {
-		if (getCached) return res.json (myCached);
-		var program = req.params.program || '';
-		sails.log.debug ('gathering issues for program: '+program);
-		// console.log ('gathering issues for program: '+program);
-		ProgramService.getIssues (program)
-		.then (ProgramService.categorizeIssues)
-		.then (aok (res), nok (res));
-	},
-	cards: function (req, res) {
-		TrelloBoardService.getCardsForBoard (req.param ('board'))
-		.then (aok (res), nok (res));
-	},
-	lists: function (req, res) {
-		TrelloBoardService.getListsForBoard (req.param ('board'))
-		.then (aok (res), nok (res));
-	},
-	listdist: function (req, res) {
-		TrelloBoardService.getListDistribution (req.param ('board'))
-		.then (aok (res), nok (res));
-		.then (function (results) {
-			sails.log.debug ('about to send results');
-			res.json (results);
-		})
-		.catch (function (err) {
-			res.json (err);
-		});
-	}
+    openIssues: function (req, res) {
+        sails.log.debug("getCommits request body", req.param('widget'));
+        var widget = req.param('widget');
+        GithubService.getCommits(widget, function (err, data, headers) {
+            return res.json(data);
+        });
+    },
+    issues: function (req, res) {
+        if (getCached) return res.json (myCached);
+        var program = req.params.program || '';
+        sails.log.debug ('gathering issues for program: ' + program);
+        ProgramService.getIssues (program)
+            .then (ProgramService.categorizeIssues)
+            .then (aok(res), nok(res));
+    },
+    cards: function (req, res) {
+        TrelloBoardService.getCardsForBoard (req.param ('board'))
+            .then (aok(res), nok(res));
+    },
+    lists: function (req, res) {
+        TrelloBoardService.getListsForBoard (req.param ('board'))
+            .then (aok(res), nok(res));
+    },
+    listdist: function (req, res) {
+        TrelloBoardService.getListDistribution (req.param ('board'))
+            .then (aok(res), nok(res));
+    }
 };
 
