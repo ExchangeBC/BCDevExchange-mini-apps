8e0d9c8882cddd41c48738249fb462f8a6d46fd1
Shea Phillips <shea.phillips@gmail.com>
2016-09-28 08:06:31 -0700

    - added sails hook for request logging (timing)

---
+++ config/requestloggerfile.js
@@ -0,0 +1,10 @@
+/**
+ * Created by wombat on 2016-09-28.
+ */
+
+module.exports.requestloggerfile = {
+
+    // override default, and turn morgan logging on in production environment
+    inProduction: true
+
+};
